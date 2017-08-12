c1c2544de2c0e05d63dee6b842b79af9802ed8f1
Kanak Vasudeva <vasudevakanak@gmail.com>
2017-08-12 14:28:41 +0530

    Autogeneration of id

--- src/main/java/com/example/hibernateExample/application/User.java
+++ src/main/java/com/example/hibernateExample/application/User.java
@@ -1,14 +1,23 @@
 package com.example.hibernateExample.application;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "User")
 public class User {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "id")
+	@NotNull
 	int id;
 	String name;
 	
